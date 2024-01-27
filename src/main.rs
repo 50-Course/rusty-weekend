391ba8693c6ea890c45819a220df8e641590cffc
50-Course <erioluwaadeodu@my.uopeople.edu>
2024-01-27 14:09:17 +0100

    getting down the basics, `functions` & `refs`

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
 fn main() {
     println!("Hello, world!");
+    println!("{}", greet("Rust".to_string()));
     print!("Happy Weekend!");
+}
+
+fn greet(_arg: String) -> String {
+    return "Hello, ".to_string() + &_arg;
 }
